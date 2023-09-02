a3020d729b0c167743c755ab48f007bf59609495
sdg419 <suhdonggeun@gmail.com>
2023-09-02 17:52:57 +0900

    first commit

---
+++ src/test/java/com/dg/springsecurityotp/SpringSecurityOtpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dg.springsecurityotp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
